79ca3406fadc4412c05e39d200ce07dee3532415
lemonmeng1995 <750516311@qq.com>
2019-11-25 02:11:17 +0800

    1.框架搭建

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="dashboard-container">
+    <!-- <div class="dashboard-text">name: {{ name }}</div> -->
+ 属性方式：
+  <el-input
+    placeholder="请选择日期"
+    suffix-icon="el-icon-date"
+    v-model="input1">
+  </el-input>
+      <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="到期时间"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="手机号"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="续费选项">
+    </el-table-column>
+      <el-table-column
+      label="续费日期">
+      <template slot-scope="scope">
+        <el-date-picker
+      v-model="value1"
+      type="date"
+      @click="handleClick(scope.row)"
+      placeholder="选择日期">
+    </el-date-picker>
+      </template>
+    </el-table-column>
+      <el-table-column
+     label="操作">
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">确认</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  data(){
+    return{
+       tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
